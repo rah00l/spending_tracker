31bb12cbbcb3fcd7ed5bf26ea657cabfa281d8fb
Rahul Patil <rahulpatil2387@gmail.com>
2015-07-05 04:03:55 +0530

    Dev-8 - Added schema info commented table in Tag Model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,12 @@
 class Tag < ActiveRecord::Base
+
+  ############################# Scema Information ##############################################################
+  # 	Column 			Type 			Collation 		Null 	Default 	Extra
+  # 1 	id 			int(11) 						No 		None 	AUTO_INCREMENT
+  # 2 	name 		varchar(255) 	utf8_unicode_ci Yes 	NULL
+  # 3 	description text 			utf8_unicode_ci Yes 	NULL
+  # 4 	created_at 	datetime 						No 		None
+  # 5 	updated_at 	datetime 						No 		None
+  ############################# Scema Information ##############################################################
+
 end
