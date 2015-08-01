afa6433f7499b0fe7ec785320dcc6e3edfa42e4b
Rahul Patil <rahulpatil2387@gmail.com>
2015-08-02 01:06:39 +0530

    Dev-33 - Permites new account_id attribute in incomes comtroller

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -69,6 +69,6 @@ class IncomesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def income_params
-      params.require(:income).permit(:date_of_income, :amount, :tag_name)
+      params.require(:income).permit(:date_of_income, :amount, :tag_name, :account_id)
     end
 end
