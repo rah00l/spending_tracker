3a58bb7aaef3511b515c6a0f02aa6d49646f0558
Rahul Patil <rahulpatil2387@gmail.com>
2015-08-16 04:30:44 +0530

    Dev-38 - Updates schema-info with account_id addition of it

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,12 +1,12 @@
 class Expense < ActiveRecord::Base
 
   ############################# Scema Information ##############################################################
-  # #	Column					Type	Collation	Null		Default		Extra
-  # 	 1	id					int(11)				No			None		AUTO_INCREMENT
-  # 	 2	date_of_expense		date				Yes			NULL
-  # 	 3	amount				float				Yes			NULL
-  # 	 4	created_at			datetime			No			None
-  # 	 5	updated_at			datetime			No			None
+  # # Column          Type  Collation Null    Default   Extra
+  #    1  id          int(11)       No      None    AUTO_INCREMENT
+  #    2  date_of_expense   date        Yes     NULL
+  #    3  amount        float       Yes     NULL
+  #    4  created_at      datetime      No      None
+  #    5  updated_at      datetime      No      None
   #       account_id      int(11)       Yes NULL
   ############################# Scema Information ##############################################################
 
