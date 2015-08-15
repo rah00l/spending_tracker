536df6f52a60486ea20a52cdb9f5d7ed133f59dd
Rahul Patil <rahulpatil2387@gmail.com>
2015-08-16 04:28:28 +0530

    Dev-38 - Updates transact#index action in cotroller

--- app/controllers/transacts_controller.rb
+++ app/controllers/transacts_controller.rb
@@ -4,9 +4,25 @@ class TransactsController < ApplicationController
   # GET /transacts
   # GET /transacts.json
   def index
-    @transacts = Transact.all
-    @income = Transact.collective_income
-    @expense = Transact.collective_expense
+    account_id = params[:account_id].nil? ? Transact.pluck(:account_id).uniq : params[:account_id].to_i
+
+    if params[:selected_option] == "Weekly"
+      @transacts = Transact.weekly.by_account(account_id)
+      @income = Transact.income.weekly.by_account(account_id).sum(:amount)
+      @expense = Transact.expense.weekly.by_account(account_id).sum(:amount)
+    elsif params[:selected_option] == "Monthly"
+      @transacts = Transact.monthly.by_account(account_id)
+      @income = Transact.income.monthly.by_account(account_id).sum(:amount)
+      @expense = Transact.expense.monthly.by_account(account_id).sum(:amount)
+    elsif params[:selected_option] == "Yearly"
+      @transacts = Transact.yearly.by_account(account_id)
+      @income = Transact.income.yearly.by_account(account_id).sum(:amount)
+      @expense = Transact.expense.yearly.by_account(account_id).sum(:amount)
+    else
+      @transacts = Transact.monthly.by_account(account_id)
+      @income = Transact.income.monthly.by_account(account_id).sum(:amount)
+      @expense = Transact.expense.monthly.by_account(account_id).sum(:amount)
+    end
   end
 
   # GET /transacts/1
