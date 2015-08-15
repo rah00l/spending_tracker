8ed905cfd7f0f0f1c0f0436181ddc644ecc03410
Rahul Patil <rahulpatil2387@gmail.com>
2015-08-16 04:23:53 +0530

    Dev-38 - Updates ajax call and also added overlay effect for ajax proccessing

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,13 +1,28 @@
 // Js file to write custom application-specific javascript.
 
 function observeAutocompleteField(fieldId, url, options) {
-  $(document).ready(function() {
-    $('#'+fieldId).autocomplete($.extend({
-      source: url,
-      minLength: 2,
-      search: function(){$('#'+fieldId).addClass('ajax-loading');},
-      response: function(){$('#'+fieldId).removeClass('ajax-loading');}
-    }, options));
-    $('#'+fieldId).addClass('autocomplete');
-  });
+	$(document).ready(function() {
+		$('#'+fieldId).autocomplete($.extend({
+			source: url,
+			minLength: 2,
+			search: function(){$('#'+fieldId).addClass('ajax-loading');},
+			response: function(){$('#'+fieldId).removeClass('ajax-loading');}
+		}, options));
+		$('#'+fieldId).addClass('autocomplete');
+	});
+}
+
+
+function update_transactions_div(selected_option){
+	$('.loadingOverlay').show();
+	$.ajax({
+		url: "/transacts",
+		type: "GET",
+		data: {"selected_option" : selected_option},
+		dataType: "script",
+		cache: true,
+		success: function(data) {
+			$('.loadingOverlay').hide();
+		}
+	});
 }
