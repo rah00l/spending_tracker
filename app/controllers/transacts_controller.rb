e0d9f613baf04ed6aad7e4b3173431071f027e9d
Rahul Patil <rahulpatil2387@gmail.com>
2015-08-23 00:50:10 +0530

    Dev-38 - Modifies in transact#index to handle both aoount and selected duration

--- app/controllers/transacts_controller.rb
+++ app/controllers/transacts_controller.rb
@@ -4,22 +4,22 @@ class TransactsController < ApplicationController
   # GET /transacts
   # GET /transacts.json
   def index
-    account_id = params[:account_id].nil? ? Transact.pluck(:account_id).uniq : params[:account_id].to_i
+    account_id = params[:account_id].nil? ? Account.first.id : params[:account_id].to_i
 
-    if params[:selected_option] == "Weekly"
+   if params[:duration] == "Weekly"
       @transacts = Transact.weekly.by_account(account_id)
       @income = Transact.income.weekly.by_account(account_id).sum(:amount)
       @expense = Transact.expense.weekly.by_account(account_id).sum(:amount)
-    elsif params[:selected_option] == "Monthly"
+    elsif params[:duration] == "Monthly"
       @transacts = Transact.monthly.by_account(account_id)
       @income = Transact.income.monthly.by_account(account_id).sum(:amount)
       @expense = Transact.expense.monthly.by_account(account_id).sum(:amount)
-    elsif params[:selected_option] == "Yearly"
+    elsif params[:duration] == "Yearly"
       @transacts = Transact.yearly.by_account(account_id)
       @income = Transact.income.yearly.by_account(account_id).sum(:amount)
       @expense = Transact.expense.yearly.by_account(account_id).sum(:amount)
     else
-      @transacts = Transact.monthly.by_account(account_id)
+      @transacts = Transact.includes(:transactable).monthly.by_account(account_id)
       @income = Transact.income.monthly.by_account(account_id).sum(:amount)
       @expense = Transact.expense.monthly.by_account(account_id).sum(:amount)
     end
@@ -80,13 +80,17 @@ class TransactsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_transact
-      @transact = Transact.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_transact
+    @transact = Transact.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def transact_params
-      params.require(:transact).permit(:transactable_id, :transactable_type, :amount)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def transact_params
+    params.require(:transact).permit(:transactable_id, :transactable_type, :amount)
+  end
+
+  def get_filter_option
+
+  end
 end
