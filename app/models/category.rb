9b0d7e82b7dbdf6c68a1717675d5e0ed637c89d7
Rahul Patil <rahulpatil2387@gmail.com>
2015-08-29 10:31:53 +0530

    Dev-41 - Adds category controller,model and helper

---
+++ app/models/category.rb
@@ -0,0 +1,18 @@
+class Category < ActiveRecord::Base
+	############################# Scema Information ##############################################################
+	# 	Column 			Type 			Collation 		Null 	Default 	Extra
+	# 1 	id 			int(11) 						No 		None 	AUTO_INCREMENT
+	# 2 	name 		varchar(255) 	utf8_unicode_ci Yes 	NULL
+	# 3 	description text 			utf8_unicode_ci Yes 	NULL
+	# 4 	created_at 	datetime 						No 		None
+	# 5 	updated_at 	datetime 						No 		None
+	############################# Scema Information ##############################################################
+
+	# Validation
+	validates :name, presence: true, uniqueness: true
+
+
+	has_many :incomes
+	has_many :expenses
+
+end
