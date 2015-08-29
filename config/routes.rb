69573d00d21727e6de6df4f0651b3e527306315a
Rahul Patil <rahulpatil2387@gmail.com>
2015-08-29 23:03:06 +0530

    Dev-40 - Updates routes and en local file changes related to remove tags

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,6 @@ Rails.application.routes.draw do
   resources :expenses
   resources :accounts
   resources :incomes
-
-  resources :tags do
-    collection do
-      get 'search'
-    end
-  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
