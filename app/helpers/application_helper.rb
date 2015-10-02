5318770ccdc146c0a65f0eb0764079972e9b7c47
Rahul Patil <rahulpatil2387@gmail.com>
2015-10-03 04:32:46 +0530

    Dev-54 - Adds few helper methods to created links for glob. selection option

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+ def account_link(account)
+	return link_to( account.name, transacts_path(account_id: account) )
+  end
+
+  def weekly_spendings(account,duration)
+    return link_to( "Weekly", transacts_path(account_id: account, duration: duration))
+  end
+
+  def monthly_spendings(account,duration)
+    return link_to( "Monthly", transacts_path(account_id: account, duration: duration))
+  end
+
+  def yearly_spendings(account,duration)
+    return link_to( "Yearly", transacts_path(account_id: account, duration: duration))
+  end
+
 end
