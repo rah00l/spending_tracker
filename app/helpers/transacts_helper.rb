a01bb22f51ddb58cf74e6db7dea8460e41ac7ce0
Rahul Patil <rahulpatil2387@gmail.com>
2015-10-04 00:24:10 +0530

    Dev-54 - Indents properly

--- app/helpers/transacts_helper.rb
+++ app/helpers/transacts_helper.rb
@@ -1,12 +1,12 @@
 module TransactsHelper
 
   def format_number_to_currency(number, html=true)
-  	# txt = html ? content_tag(:span, '', class: 'rupee') : 'Rs.'
+    # txt = html ? content_tag(:span, '', class: 'rupee') : 'Rs.'
     " #{number.to_s.gsub(/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/, "\\1,")}"
   end
 
   def currency_color(type)
-  	type.eql?('Income') ? 'income_amount' : 'expense_amount'
+    type.eql?('Income') ? 'income_amount' : 'expense_amount'
   end
 
   def get_duration_info(duration)
