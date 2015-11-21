d244b0587e89345619d25d5991dcf20c0c561e53
Rahul Patil <rahulpatil2387@gmail.com>
2015-11-22 01:24:03 +0530

    Dev-55 - Adds admin_user authenticate method in applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,19 @@ class ApplicationController < ActionController::Base
 
   before_action :account_info
 
+  def authenticate_admin_user!
+    authenticate_user!
+    unless current_user.admin?
+      flash[:alert] = "This area is restricted to administrators only."
+      redirect_to root_path
+    end
+  end
+
+  def current_admin_user
+    return nil if user_signed_in? && !current_user.admin?
+    current_user
+  end
+
   private
 
   def account_info
