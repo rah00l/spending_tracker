5322c3545d458b3d4f1ae98b63e69cab6d942c04
Rahul Patil <rahulpatil2387@gmail.com>
2015-11-28 20:38:44 +0530

    Dev-56 - Add quote association with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # defination of association macro's
+  has_many :qoutes, dependent: :destroy
 end
