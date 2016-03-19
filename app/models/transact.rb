4d2f6ba80a6ddbe5319a68cf8108a17df9c7bb04
Rahul Patil <rahulpatil2387@gmail.com>
2016-03-20 04:38:54 +0530

    Dev-59 - Add filtered_result and filter_data with scope for Transact model

--- app/models/transact.rb
+++ app/models/transact.rb
@@ -24,4 +24,37 @@ class Transact < ActiveRecord::Base
   scope :monthly, -> { where('created_at>= ? and created_at<=?', Date.today.beginning_of_month, Date.today.end_of_month) }
   scope :yearly, -> { where('created_at>= ? and created_at<=?', Date.today.beginning_of_year, Date.today.end_of_year) }
 
+  scope :filter_option, -> (key, value) {
+                              where("transacts.#{key} = ?", value)
+                            }
+  scope :range_filter, ->(key, value_1, value_2) {
+                            where(:"transacts.#{key}" => value_1..value_2)
+                          }
+
+  def self.filtered_result(options)
+    # options['sort'] = 'file_details.updated_at' if options['sort'].blank?
+    # options['direction'] = 'DESC' if options['direction'].blank?
+
+    result = includes(transactable: :category)
+              .filter_data(options)
+              # .order("#{options['sort']} #{options['direction']}")
+
+    # result = result.unique_file_details if options['unique_file_details'].present?
+    result
+  end
+
+  def self.filter_data(options)
+    result = self
+    column_names.each do |name|
+        if options[name].present? && %w(updated_at).exclude?(name)
+        result = result.filter_option(name, options[name])
+      end
+    end
+
+    result = result.range_filter('created_at', options[:from], options[:to])
+    # result = result.page_count(prepare_page_count(options)) if options['page_count'].present?
+
+    result
+  end
+
 end
